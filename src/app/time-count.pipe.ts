944a45efdccd9d48b467fa00c090a641afef236e
Alfred Warui <alfredwarui17@gmail.com>
2021-08-20 00:56:21 +0300

    create a custom pipe to count time elapsed since creation of quote

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayTimeless:any =new Date(today.getFullYear(),today.getMonth(),today.getDate());
+    var dateDifference = Math.abs(todayTimeless - value);
+    const daysSeconds = 86400;
+    var dateDifferenceSeconds = dateDifference * 0.001;
+    var timeCounter = dateDifferenceSeconds/daysSeconds;
+    var absoluteTime = parseInt(timeCounter.toFixed());
+
+    if(absoluteTime >= 1 && todayTimeless > value) {
+      return absoluteTime;
+    } else {
+      return 0;
+    }
   }
 
 }
