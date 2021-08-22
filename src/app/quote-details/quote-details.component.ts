e2bec3f2720fa7287e231b53ad5c68af5d06cb9b
Alfred Warui <alfredwarui17@gmail.com>
2021-08-22 15:10:46 +0300

    add quote details folder containing quote details component for displaying quote details

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { QuoteBlueprint } from './../quote-blueprint';
+import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import { faTrash, faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-quote-details',
@@ -7,6 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailsComponent implements OnInit {
 
+  faTrash = faTrash;
+  faThumbsUp = faThumbsUp;
+  faThumbsDown = faThumbsDown;
+
+  @Input() quote!:QuoteBlueprint;
+  @Output() uninterested = new EventEmitter<boolean> ();
+  @Output() upVote = new EventEmitter<boolean> ();
+  @Output() downVote = new EventEmitter<boolean> ();
+
+  quoteDelete(useless:boolean){
+    this.uninterested.emit(useless);
+  }
+
+  like(votePlus:boolean){
+    this.upVote.emit(votePlus);
+  }
+  dislike(voteMinus:boolean){
+    this.downVote.emit(voteMinus);
+  }
   constructor() { }
 
   ngOnInit(): void {
