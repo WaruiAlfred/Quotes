7f84880e5f0b440ecd363976603a348a4bfeee99
Alfred Warui <alfredwarui17@gmail.com>
2021-08-22 15:12:08 +0300

    add quote-form component for quote inputs

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { QuoteBlueprint } from './../quote-blueprint';
 
 @Component({
   selector: 'app-quote-form',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote = new QuoteBlueprint(0,"","","",new Date(),0,0);
+  @Output() addQuote = new EventEmitter<QuoteBlueprint>();
+
+
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);
+    this.newQuote = new QuoteBlueprint(0,"","","",new Date(),0,0);
+  }
   constructor() { }
 
   ngOnInit(): void {
