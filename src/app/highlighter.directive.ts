9495ea5c707d82783e7ae29b04cf8e76268350fc
Alfred Warui <alfredwarui17@gmail.com>
2021-08-22 14:54:09 +0300

    create a directive to highlight quote with highest upvotes

--- src/app/highlighter.directive.ts
+++ src/app/highlighter.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
+import { QuoteBlueprint } from './quote-blueprint';
 
 @Directive({
   selector: '[appHighlighter]'
 })
 export class HighlighterDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) {
+    this.elem.nativeElement.style.backgroundColor="blue";
+
+  }
 
 }
