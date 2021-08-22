764e3b661d53e13e1f996c29f66529e1f2ef42e4
Alfred Warui <alfredwarui17@gmail.com>
2021-08-22 14:52:36 +0300

    create the quotes blueprint

--- src/app/quote-blueprint.ts
+++ src/app/quote-blueprint.ts
@@ -1,2 +1,7 @@
 export class QuoteBlueprint {
+
+  public showDetails:boolean;
+  constructor(public id:number, public quoteDetail:string, public author:string, public submitter:string,public inputDate:Date, public upVotes:number, public downVotes:number) {
+    this.showDetails = false;
+  }
 }
